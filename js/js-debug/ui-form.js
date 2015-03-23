0fbc5c5f719cccf0d0ae9460aa544693d1604272
xyao <xyao201@qq.com>
2015-03-23 17:06:56 +0800

    模拟下拉

--- js/js-debug/ui-form.js
+++ js/js-debug/ui-form.js
@@ -1,9 +1,31 @@
 ! function($) {
-	$.fn.galaxy.formInit = function () {
+	$.fn.galaxy.formInit = function() {
 		var select = $(".ui-form-select")
 		if (select.length == 0) {
 			return false;
 		};
+		wrapSelect(select);
+		initSelect(select)
+		var tSelect = $(".ui-select-wrap");
+		var value = select.val();
+	}
+
+	function wrapSelect(select) {
 		select.wrap("<div class=ui-select-wrap></div>");
+		var text = "<div class='ui-select-value'></div><div class='ui-select-arr'></div>"
+		$(select).before(text);
+	}
+
+	function initSelect(select) {
+		$(".ui-select-wrap").each(function(index) {
+			$(this).find(".ui-select-value").text(selecText($(this)));
+		});
+		select.on("change",function(){
+			var el = $(this);
+			el.parent().find(".ui-select-value").text(selecText(el));
+		})
+	}
+	function selecText($el){
+		return $el.find("option").not(function(){ return !this.selected }).text();
 	}
 }(window.Zepto)
